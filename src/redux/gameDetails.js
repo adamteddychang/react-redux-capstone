720a7c5a6834236abe02d532ddf42a0b55956ac2
adamteddychang <adamchang0725@gmail.com>
2021-12-21 10:24:02 +0100

    initialized game, game details store

---
+++ src/redux/gameDetails.js
@@ -0,0 +1,8 @@
+const ADD_GAME_DETAILS = 'ADD_GAME_DETAILS'
+
+const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
+const initialState = {};
+
+const addGameDetails = (payload) = ({
+
+})
---
+++ src/redux/gamesAPI.js
@@ -0,0 +1,33 @@
+const ADD_GAMES_LIST_TO_STORE = 'ADD_GAMES_LIST_TO_STORE';
+
+const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
+
+const initialState = [];
+
+const addGamesListToStore = (payload) => ({
+  type: ADD_GAMES_LIST_TO_STORE,
+  payload,
+});
+
+const getGameList = () =>(dispatch) => {
+  fetch(url, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+		"x-rapidapi-key": "b2ff50aa96mshb062ddb7e5817c1p1cea6djsna1e9d633dcca"
+	}
+}).then((response) => response.json())
+.then((games) => {
+  dispatch(addGamesListToStore(games));
+});
+}
+
+const gamesReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_GAMES_LIST_TO_STORE:
+      return action.payload;
+      default: return state;
+  }
+}
+
+export{gamesReducer as default, getGameList}
