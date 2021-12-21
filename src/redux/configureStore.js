d27f7d158d102432d79f100a16a0b559dacf449e
adamteddychang <adamchang0725@gmail.com>
2021-12-21 10:32:42 +0100

    completed get game details store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import gamesReducer from './gamesAPI';
+import detailsReducer from './gameDetails'
 
 const reducer = combineReducers({
   games: gamesReducer,
+  details: detailsReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
--- src/redux/gameDetails.js
+++ src/redux/gameDetails.js
@@ -2,7 +2,33 @@ const ADD_GAME_DETAILS = 'ADD_GAME_DETAILS'
 
 const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
 const initialState = {};
+let payload;
+let id ;
 
 const addGameDetails = (payload) = ({
+  type: ADD_GAME_DETAILS,
+  payload,
+});
 
-})
+const getGameDetails = (id) = (dispatch) => {
+  fetch(`${url}?id=${id}`, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+      "x-rapidapi-key": "b2ff50aa96mshb062ddb7e5817c1p1cea6djsna1e9d633dcca",
+    },
+}).then((response) => response.json())
+.then((game) => {
+  dispatch(addGameDetails(game));
+});
+};
+
+const detailsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_GAME_DETAILS:     
+    return action.payload;
+    default: return state;
+  }
+};
+
+export {detailsReducer as default, getGameDetails};
