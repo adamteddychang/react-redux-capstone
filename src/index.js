84630f0f090a5fac8d3dbfade1b784720661482f
adamteddychang <adamchang0725@gmail.com>
2021-12-21 10:20:31 +0100

    initialized redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App'
+import store from './redux/configureStore'
 import './index.css';
-import App from './App';
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
+  document.getElementById('root'),
+); 
